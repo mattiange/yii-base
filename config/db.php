8c83225d47d30a625fda362b275fd250fa4c23b8
Rino Andriano <andriano@itccolamonico.it>
2015-03-30 16:03:35 +0200

    Yii base -  Tutorial - initial commit (menu)

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2prova',
+    'username' => 'phpuser',
+    'password' => 'phppwd',
+    'charset' => 'utf8',
+];
